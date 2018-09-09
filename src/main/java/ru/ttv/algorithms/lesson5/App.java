25bcff6a76669f7166a480f068b169b82137b26d
timteplych <timteplych@mail.ru>
2018-09-09 15:48:19 +0800

    lesson 5

---
+++ src/main/java/ru/ttv/algorithms/lesson5/App.java
@@ -0,0 +1,9 @@
+package ru.ttv.algorithms.lesson5;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+
+
+}
---
+++ src/main/java/ru/ttv/algorithms/lesson5/HanoiTowers.java
@@ -0,0 +1,13 @@
+package ru.ttv.algorithms.lesson5;
+
+public class HanoiTowers {
+    private int disksAmount;
+
+    public HanoiTowers(int disksAmount) {
+        this.disksAmount = disksAmount;
+    }
+
+    public void start(){
+        
+    }
+}
